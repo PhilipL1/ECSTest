c919cfb7767dc5542ac5ace80571942c01d095ed
Philip <Philiplartey1@hotmail.com>
2021-09-17 15:22:10 +0100

    1st trail

--- python/medals.py
+++ python/medals.py
@@ -20,7 +20,18 @@ medalResults = [
 def createMedalTable(results):
     # Use the results object above to create a medal table
     # The winner gets 3 points, second place 2 points and third place 1 point
-    return
+    points = { "1" : 3, "2" : 2, "3": 1}
+    res = DefaultDict(int)
+    for dictionary in results:
+        for key, value in dictionary.items():
+            if key == "podium":
+                for places in value:
+                    parts = places.split(".")
+                    position = parts[0]
+                    country = parts[1]
+                    res[country] += points[position]
+    return res
+
 
 
 def test_function():
